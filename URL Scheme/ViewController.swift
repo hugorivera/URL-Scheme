b46c4474dc783dbe34230042b56504992ad638e1
Hugo Rivera <riveraquevedoh@gmail.com>
2023-12-17 12:42:35 -0600

    Initial Commit

---
+++ URL Scheme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URL Scheme
+//
+//  Created by Hugo Francisco Rivera Quevedo on 17/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
